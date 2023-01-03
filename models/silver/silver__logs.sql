e53dad4ee79cf65c52a0ebd3f5d4a96fce046ed6
drethereum <71602799+drethereum@users.noreply.github.com>
2023-01-03 14:07:33 -0700

    AN-2580/unique-key-tx-test (#35)
    * changed unique key in logs and traces and added new test to logs
    
    * remove user file
    
    * added dbt-env to gitignore for SSO functionality

--- models/silver/silver__logs.sql
+++ models/silver/silver__logs.sql
@@ -1,7 +1,8 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = "_log_id",
-    cluster_by = ['_inserted_timestamp::DATE']
+    unique_key = 'tx_hash',
+    cluster_by = ['_inserted_timestamp::DATE'],
+    incremental_strategy = 'delete+insert'
 ) }}
 
 WITH base_txs AS (

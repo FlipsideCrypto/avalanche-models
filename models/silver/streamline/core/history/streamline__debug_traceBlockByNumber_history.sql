68e2e204c6681cd4da0b784fd2c6207beba3a829
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-04-14 14:31:22 -0400

    Explode updates
    - Updated formatting
    - Created folders for streamline
    - Combined calls in GH actions
    - Set range for backfill

--- models/silver/streamline/core/history/streamline__debug_traceBlockByNumber_history.sql
+++ models/silver/streamline/core/history/streamline__debug_traceBlockByNumber_history.sql
@@ -1,17 +1,17 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_get_traces(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'debug_traceBlockByNumber', 'sql_limit', {{var('sql_limit','150000')}}, 'producer_batch_size', {{var('producer_batch_size','150000')}}, 'worker_batch_size', {{var('worker_batch_size','500')}}, 'batch_call_limit', {{var('batch_call_limit','1')}}))",
+        func = "{{this.schema}}.udf_bulk_get_traces(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'debug_traceBlockByNumber', 'sql_limit', {{var('sql_limit','120000')}}, 'producer_batch_size', {{var('producer_batch_size','120000')}}, 'worker_batch_size', {{var('worker_batch_size','300')}}, 'batch_call_limit', {{var('batch_call_limit','1')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
 
-{% for item in range(29) %}
+{% for item in range(28) %}
     (
         WITH blocks AS (
 
             SELECT
-                block_number :: STRING AS block_number
+                block_number
             FROM
                 {{ ref("streamline__blocks") }}
             WHERE
@@ -21,7 +21,7 @@
                 ) * 1000000 }}
             EXCEPT
             SELECT
-                block_number :: STRING
+                block_number
             FROM
                 {{ ref("streamline__complete_debug_traceBlockByNumber") }}
             WHERE
@@ -34,9 +34,20 @@
             PARSE_JSON(
                 CONCAT(
                     '{"jsonrpc": "2.0",',
-                    '"method": "debug_traceBlockByNumber", "params":[',
-                    block_number :: INTEGER,
-                    ',{"tracer": "callTracer"}',
+                    '"method": "debug_traceBlockByNumber", "params":["',
+                    REPLACE(
+                        concat_ws(
+                            '',
+                            '0x',
+                            to_char(
+                                block_number :: INTEGER,
+                                'XXXXXXXX'
+                            )
+                        ),
+                        ' ',
+                        ''
+                    ),
+                    '",{"tracer": "callTracer"}',
                     '],"id":"',
                     block_number :: STRING,
                     '"}'
@@ -44,6 +55,8 @@
             ) AS request
         FROM
             blocks
+        ORDER BY
+            block_number ASC
     ) {% if not loop.last %}
     UNION ALL
     {% endif %}
--- models/silver/streamline/core/realtime/streamline__qn_getBlockWithReceipts_realtime.sql
+++ models/silver/streamline/core/realtime/streamline__qn_getBlockWithReceipts_realtime.sql
@@ -5,17 +5,16 @@
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
+{# WITH last_3_days AS (
 
-WITH last_3_days AS (
-
-    SELECT
-        block_number
-    FROM
-        {{ ref("_max_block_by_date") }}
-        qualify ROW_NUMBER() over (
-            ORDER BY
-                block_number DESC
-        ) = 3
+SELECT
+    block_number
+FROM
+    {{ ref("_max_block_by_date") }}
+    qualify ROW_NUMBER() over (
+        ORDER BY
+            block_number DESC
+    ) = 3
 ),
 blocks AS (
     SELECT
@@ -45,17 +44,46 @@ blocks AS (
                     last_3_days
             )
         )
+) #}
+WITH blocks AS (
+    SELECT
+        block_number :: STRING AS block_number
+    FROM
+        {{ ref("streamline__blocks") }}
+    WHERE
+        block_number >= 28000000
+    EXCEPT
+    SELECT
+        block_number :: STRING
+    FROM
+        {{ ref("streamline__complete_qn_getBlockWithReceipts") }}
+    WHERE
+        block_number >= 28000000
+        and block_number <= 28100000
 )
 SELECT
     PARSE_JSON(
         CONCAT(
             '{"jsonrpc": "2.0",',
-            '"method": "qn_getBlockWithReceipts", "params":[',
-            block_number :: INTEGER,
-            '],"id":"',
+            '"method": "qn_getBlockWithReceipts", "params":["',
+            REPLACE(
+                concat_ws(
+                    '',
+                    '0x',
+                    to_char(
+                        block_number :: INTEGER,
+                        'XXXXXXXX'
+                    )
+                ),
+                ' ',
+                ''
+            ),
+            '"],"id":"',
             block_number :: STRING,
             '"}'
         )
     ) AS request
 FROM
     blocks
+ORDER BY
+    block_number ASC

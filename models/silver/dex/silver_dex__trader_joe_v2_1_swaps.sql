43836b79b3eea052d9e538419f41b1adf755b590
drethereum <trevor.wenokur@gmail.com>
2023-04-18 12:59:44 -0600

    trader joe v2 and v2.1 pools and swaps

--- models/silver/dex/silver_dex__trader_joe_v2_1_swaps.sql
+++ models/silver/dex/silver_dex__trader_joe_v2_1_swaps.sql
@@ -9,56 +9,78 @@
 WITH pools AS (
 
     SELECT
-        pool_address,
-        token0,
-        token1
+        lb_pair,
+        tokenX,
+        tokenY,
+        version
     FROM
-        {{ ref('silver_dex__trader_joe_v1_pools') }}
+        {{ ref('silver_dex__trader_joe_v2_pools') }}
 ),
 swaps_base AS (
     SELECT
         l.block_number,
         l.block_timestamp,
+        l.origin_function_signature,
+        l.origin_from_address,
+        l.origin_to_address,
         l.tx_hash,
-        origin_function_signature,
-        origin_from_address,
-        origin_to_address,
         l.event_index,
         l.contract_address,
-        regexp_substr_all(SUBSTR(l.data, 3, len(l.data)), '.{64}') AS l_segmented_data,
+        l.topics,
+        l.data,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS l_segmented_data,
         CONCAT('0x', SUBSTR(l.topics [1] :: STRING, 27, 40)) AS sender_address,
         CONCAT('0x', SUBSTR(l.topics [2] :: STRING, 27, 40)) AS to_address,
         TRY_TO_NUMBER(
             ethereum.public.udf_hex_to_int(
                 l_segmented_data [0] :: STRING
             )
+        ) AS id,
+        l_segmented_data [1] :: STRING AS amountsIn,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(SUBSTR(amountsIn, 0, 32))
         ) AS amount0In,
         TRY_TO_NUMBER(
-            ethereum.public.udf_hex_to_int(
-                l_segmented_data [1] :: STRING
-            )
+            ethereum.public.udf_hex_to_int(SUBSTR(amountsIn, 33, 32))
         ) AS amount1In,
+        l_segmented_data [2] :: STRING AS amountsOut,
         TRY_TO_NUMBER(
-            ethereum.public.udf_hex_to_int(
-                l_segmented_data [2] :: STRING
-            )
+            ethereum.public.udf_hex_to_int(SUBSTR(amountsOut, 0, 32))
         ) AS amount0Out,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(SUBSTR(amountsOut, 33, 32))
+        ) AS amount1Out,
         TRY_TO_NUMBER(
             ethereum.public.udf_hex_to_int(
                 l_segmented_data [3] :: STRING
             )
-        ) AS amount1Out,
-        token0,
-        token1,
+        ) AS volatilityAccumulated,
+        l_segmented_data [4] :: STRING AS totalFees,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(SUBSTR(totalFees, 0, 32))
+        ) AS fee0,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(SUBSTR(totalFees, 33, 32))
+        ) AS fee1,
+        l_segmented_data [5] :: STRING AS protocolFees,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(SUBSTR(protocolFees, 0, 32))
+        ) AS protocolFee0,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(SUBSTR(protocolFees, 33, 32))
+        ) AS protocolFee1,
+        tokenX,
+        tokenY,
         l._log_id,
         l._inserted_timestamp
     FROM
         {{ ref('silver__logs') }}
         l
         INNER JOIN pools p
-        ON p.pool_address = l.contract_address
+        ON lb_pair = l.contract_address
     WHERE
-        l.topics [0] :: STRING = '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822' --Swap
+        topics [0] :: STRING = '0xad7d6f97abf51ce18e17a38f4d70e975be9c0708474987bb3e26ad21bd93ca70' --Swap
+        AND version = 'v2.1'
 
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
@@ -73,19 +95,27 @@ SELECT
     block_number,
     block_timestamp,
     tx_hash,
-    contract_address,
     origin_function_signature,
     origin_from_address,
     origin_to_address,
     event_index,
+    contract_address,
+    topics,
+    DATA,
     sender_address AS sender,
     to_address AS tx_to,
+    id,
     amount0In,
     amount1In,
     amount0Out,
     amount1Out,
-    token0,
-    token1,
+    volatilityAccumulated,
+    fee0,
+    fee1,
+    protocolFee0,
+    protocolFee1,
+    tokenX,
+    tokenY,
     CASE
         WHEN amount0In <> 0
             AND amount1In <> 0
@@ -100,16 +130,16 @@ SELECT
     CASE
         WHEN amount0In <> 0
             AND amount1In <> 0
-            AND amount0Out <> 0 THEN token1
-        WHEN amount0In <> 0 THEN token0
-        WHEN amount1In <> 0 THEN token1
+            AND amount0Out <> 0 THEN tokenX
+        WHEN amount0In <> 0 THEN tokenY
+        WHEN amount1In <> 0 THEN tokenX
     END AS token_in,
     CASE
-        WHEN amount0Out <> 0 THEN token0
-        WHEN amount1Out <> 0 THEN token1
+        WHEN amount0Out <> 0 THEN tokenY
+        WHEN amount1Out <> 0 THEN tokenX
     END AS token_out,
     'Swap' AS event_name,
-    'trader-joe-v1' AS platform,
+    'trader-joe-v2.1' AS platform,
     _log_id,
     _inserted_timestamp
 FROM

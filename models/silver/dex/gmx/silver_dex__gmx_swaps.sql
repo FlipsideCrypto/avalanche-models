3cca496acca5df440015a0c03ffc840a7f23a5ae
drethereum <71602799+drethereum@users.noreply.github.com>
2023-04-20 15:37:59 -0600

    AN-3047/dex-swaps (#68)
    * trader joe v1 pools and swaps
    
    * trader joe v2 and v2.1 pools and swaps
    
    * folder reorg
    
    * woofi swaps
    
    * gmx swaps
    
    * kyberswap dynamic
    
    * kyber swap v1 and v2
    
    * pangolin and platypus swaps
    
    * fraxswap
    
    * sushi and curve
    
    * hashflow
    
    * curve and complete dex swaps
    
    * missing contracts and events added to gmx and woofi
    
    * updated pools for platypus
    
    * frax pools for missing events
    
    * hashflow event added
    
    * incremental changes
    
    * sushi pools event based

---
+++ models/silver/dex/gmx/silver_dex__gmx_swaps.sql
@@ -0,0 +1,97 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = '_log_id',
+    cluster_by = ['block_timestamp::DATE']
+) }}
+
+WITH swaps_base AS (
+
+    SELECT
+        l.block_number,
+        l.block_timestamp,
+        l.tx_hash,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        l.event_index,
+        l.contract_address,
+        regexp_substr_all(SUBSTR(l.data, 3, len(l.data)), '.{64}') AS l_segmented_data,
+        CONCAT(
+            '0x',
+            SUBSTR(
+                l_segmented_data [0] :: STRING,
+                25,
+                40
+            )
+        ) AS account_address,
+        CONCAT(
+            '0x',
+            SUBSTR(
+                l_segmented_data [1] :: STRING,
+                25,
+                40
+            )
+        ) AS tokenIn,
+        CONCAT(
+            '0x',
+            SUBSTR(
+                l_segmented_data [2] :: STRING,
+                25,
+                40
+            )
+        ) AS tokenOut,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(
+                l_segmented_data [3] :: STRING
+            )
+        ) AS amountIn,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(
+                l_segmented_data [4] :: STRING
+            )
+        ) AS amountOut,
+        l._log_id,
+        l._inserted_timestamp
+    FROM
+        {{ ref('silver__logs') }}
+        l
+    WHERE
+        contract_address IN (
+            '0x5f719c2f1095f7b9fc68a68e35b51194f4b6abe8',
+            '0x9ab2de34a33fb459b538c43f251eb825645e8595'
+        )
+        AND topics [0] :: STRING IN (
+            '0xcd3829a3813dc3cdd188fd3d01dcf3268c16be2fdd2dd21d0665418816e46062',
+            '0x0874b2d545cb271cdbda4e093020c452328b24af12382ed62c4d00f5c26709db'
+        ) --Swap
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE
+    FROM
+        {{ this }}
+)
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    event_index,
+    contract_address,
+    origin_from_address AS sender,
+    account_address AS tx_to,
+    tokenIn AS token_in,
+    tokenOut AS token_out,
+    amountIn AS amount_in_unadj,
+    amountOut AS amount_out_unadj,
+    'Swap' AS event_name,
+    'gmx' AS platform,
+    _log_id,
+    _inserted_timestamp
+FROM
+    swaps_base
---
+++ models/silver/dex/platypus/silver_dex__platypus_pools.sql
@@ -0,0 +1,49 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "pool_address"
+) }}
+
+WITH contract_deployments AS (
+
+    SELECT
+        tx_hash,
+        block_number,
+        block_timestamp,
+        from_address AS deployer_address,
+        to_address AS contract_address,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__traces') }}
+    WHERE
+        from_address = '0x416a7989a964c9ed60257b064efc3a30fe6bf2ee'
+        AND TYPE ILIKE 'create%'
+        AND tx_status ILIKE 'success'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE
+    FROM
+        {{ this }}
+)
+AND to_address NOT IN (
+    SELECT
+        DISTINCT pool_address
+    FROM
+        {{ this }}
+)
+{% endif %}
+
+qualify(ROW_NUMBER() over(PARTITION BY to_address
+ORDER BY
+    block_timestamp ASC)) = 1
+)
+
+SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
+    deployer_address,
+    contract_address AS pool_address,
+    _inserted_timestamp
+FROM contract_deployments

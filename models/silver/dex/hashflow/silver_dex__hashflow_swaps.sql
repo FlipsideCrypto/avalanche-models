99dc0df7479a37eeda8a5cc47d4653752bad5d61
drethereum <trevor.wenokur@gmail.com>
2023-04-20 13:14:54 -0600

    hashflow event added

--- models/silver/dex/hashflow/silver_dex__hashflow_swaps.sql
+++ models/silver/dex/hashflow/silver_dex__hashflow_swaps.sql
@@ -12,7 +12,7 @@ WITH pools AS (
         {{ ref('silver_dex__hashflow_pools') }}
 ),
 
-swaps_base AS (
+router_swaps_base AS (
 
     SELECT
         l.block_number,
@@ -55,7 +55,74 @@ AND _inserted_timestamp >= (
         {{ this }}
 )
 {% endif %}
+),
+
+swaps_base AS (
+
+    SELECT
+        l.block_number,
+        l.block_timestamp,
+        l.tx_hash,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        l.event_index,
+        l.contract_address,
+        regexp_substr_all(SUBSTR(l.data, 3, len(l.data)), '.{64}') AS l_segmented_data,
+        CONCAT('0x', SUBSTR(l_segmented_data [0] :: STRING, 25, 40)) AS account_address,
+        CONCAT('0x', SUBSTR(l_segmented_data [2] :: STRING, 25, 40)) AS tokenIn,
+        CONCAT('0x', SUBSTR(l_segmented_data [3] :: STRING, 25, 40)) AS tokenOut,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(
+                l_segmented_data [4] :: STRING
+            )
+        ) AS amountIn,
+        TRY_TO_NUMBER(
+            ethereum.public.udf_hex_to_int(
+                l_segmented_data [5] :: STRING
+            )
+        ) AS amountOut,
+        l._log_id,
+        l._inserted_timestamp
+    FROM
+        {{ ref('silver__logs') }}
+        l
+        INNER JOIN pools p
+        ON l.contract_address = p.pool_address
+    WHERE
+        l.topics [0] :: STRING = '0x8cf3dec1929508e5677d7db003124e74802bfba7250a572205a9986d86ca9f1e' --swap
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) :: DATE - 1
+    FROM
+        {{ this }}
 )
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    event_index,
+    contract_address,
+    origin_from_address AS sender,
+    account_address AS tx_to,
+    tokenIn AS token_in,
+    tokenOut AS token_out,
+    amountIn AS amount_in_unadj,
+    amountOut AS amount_out_unadj,
+    'Swap' AS event_name,
+    'hashflow' AS platform,
+    _log_id,
+    _inserted_timestamp
+FROM
+    router_swaps_base
+UNION ALL
 SELECT
     block_number,
     block_timestamp,

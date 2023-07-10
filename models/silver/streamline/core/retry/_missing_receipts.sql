fb63aca7474de3ac84f2f95861341d018643c03f
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-07-10 19:43:17 -0400

    fix (#123)

--- models/silver/streamline/core/retry/_missing_receipts.sql
+++ models/silver/streamline/core/retry/_missing_receipts.sql
@@ -2,30 +2,57 @@
     materialized = "ephemeral"
 ) }}
 
+WITH lookback AS (
+
+    SELECT
+        MAX(block_number) AS block_lookback
+    FROM
+        {{ ref("silver__blocks") }}
+    WHERE
+        block_timestamp :: DATE = CURRENT_DATE() - 3
+),
+txs AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        block_hash
+    FROM
+        {{ ref("silver__transactions") }}
+    WHERE
+        block_number >= (
+            SELECT
+                block_lookback
+            FROM
+                lookback
+        )
+),
+receipts AS (
+    SELECT
+        block_number,
+        tx_hash,
+        block_hash
+    FROM
+        {{ ref("silver__receipts") }}
+    WHERE
+        block_number >= (
+            SELECT
+                block_lookback
+            FROM
+                lookback
+        )
+)
 SELECT
     DISTINCT COALESCE(
-        tx.block_number,
+        t.block_number,
         r.block_number
     ) AS block_number
 FROM
-    {{ ref("silver__transactions") }}
-    tx full
-    OUTER JOIN {{ ref("silver__receipts") }}
-    r
-    ON tx.block_number = r.block_number
-    AND tx.tx_hash = r.tx_hash
-    AND tr.block_timestamp >= DATEADD(
-        'day',
-        -2,
-        CURRENT_DATE
-    )
+    txs t full
+    OUTER JOIN receipts r
+    ON t.block_number = r.block_number
+    AND t.block_hash = r.block_hash
+    AND t.tx_hash = r.tx_hash
 WHERE
-    tx.block_timestamp >= DATEADD(
-        'day',
-        -2,
-        CURRENT_DATE
-    )
-    AND (
-        r.tx_hash IS NULL
-        OR tx.tx_hash IS NULL
-    )
+    r.tx_hash IS NULL
+    OR t.tx_hash IS NULL

20754c9c7dcf3f98efb0d038aac694a8d687eba4
Austin <austin@flipsidecrypto.com>
2023-08-08 09:40:24 -0400

    retry abis

--- models/bronze/api_udf/bronze_api__contract_abis.sql
+++ models/bronze/api_udf/bronze_api__contract_abis.sql
@@ -33,30 +33,46 @@ WHERE
     abi_data :data :result :: STRING <> 'Max rate limit reached'
 {% endif %}
 LIMIT
-    100
-)
+    50
+), all_contracts AS (
+    SELECT
+        contract_address
+    FROM
+        base
+    UNION
+    SELECT
+        contract_address
+    FROM
+        {{ ref('_retry_abis') }}
+),
+row_nos AS (
+    SELECT
+        contract_address,
+        ROW_NUMBER() over (
+            ORDER BY
+                contract_address
+        ) AS row_no,
+        api_key
+    FROM
+        all_contracts
+        JOIN api_keys
+        ON 1 = 1
+),
+batched AS ({% for item in range(150) %}
+SELECT
+    rn.contract_address, ethereum.streamline.udf_api('GET', CONCAT('https://api.snowtrace.io/api?module=contract&action=getabi&address=', contract_address, '&apikey=', api_key),{},{}) AS abi_data, SYSDATE() AS _inserted_timestamp
+FROM
+    row_nos rn
+WHERE
+    row_no = {{ item }}
+
+    {% if not loop.last %}
+    UNION ALL
+    {% endif %}
+{% endfor %})
 SELECT
     contract_address,
-    ethereum.streamline.udf_api(
-        'GET',
-        CONCAT(
-            'https://api.snowtrace.io/api?module=contract&action=getabi&address=',
-            contract_address,
-            '&apikey=',
-            api_key
-        ),{},{}
-    ) AS abi_data,
-    SYSDATE() AS _inserted_timestamp
+    abi_data,
+    _inserted_timestamp
 FROM
-    base
-    LEFT JOIN api_keys
-    ON 1 = 1
-WHERE
-    EXISTS (
-        SELECT
-            1
-        FROM
-            base
-        LIMIT
-            1
-    )
+    batched

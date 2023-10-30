72322875a2e32ea638447050598cc7bb2556761e
mattromano <mattromano88@gmail.com>
2023-10-30 11:56:15 -0700

    tags update

--- models/silver/dex/hashflow/silver_dex__hashflow_v3_swaps.sql
+++ models/silver/dex/hashflow/silver_dex__hashflow_v3_swaps.sql
@@ -3,9 +3,9 @@
     incremental_strategy = 'delete+insert',
     unique_key = "block_number",
     cluster_by = ['block_timestamp::DATE'],
-    tags = ['non_realtime','reorg']
+    tags = ['curated','reorg']
 ) }}
---Check and see where and if there are router swaps
+
 WITH pools AS (
 
     SELECT

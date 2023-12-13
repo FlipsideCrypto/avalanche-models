ce5e9673cc4b181fd23b92000c708ef13e6b7a1a
Matt Romano <42412983+mattromano@users.noreply.github.com>
2023-12-13 07:26:00 -0800

    AN-4263/add-api_udf-and-key-migration  (#199)
    * add udf_api
    
    * Update bronze_api__contract_abis.sql
    
    * Update bronze_api__contract_abis.sql
    
    * update to route scan
    
    * remove explorer tag

--- models/bronze/api_udf/bronze_api__contract_abis.sql
+++ models/bronze/api_udf/bronze_api__contract_abis.sql
@@ -5,19 +5,8 @@
     tags = ['curated']
 ) }}
 
-WITH api_keys AS (
+WITH base AS (
 
-    SELECT
-        api_key
-    FROM
-        {{ source(
-            'crosschain_silver',
-            'apis_keys'
-        ) }}
-    WHERE
-        api_name = 'snowtrace'
-),
-base AS (
     SELECT
         contract_address
     FROM
@@ -51,16 +40,14 @@ row_nos AS (
         ROW_NUMBER() over (
             ORDER BY
                 contract_address
-        ) AS row_no,
-        api_key
+        ) AS row_no
     FROM
         all_contracts
-        JOIN api_keys
-        ON 1 = 1
 ),
 batched AS ({% for item in range(101) %}
 SELECT
-    rn.contract_address, ethereum.streamline.udf_api('GET', CONCAT('https://api.snowtrace.io/api?module=contract&action=getabi&address=', contract_address, '&apikey=', api_key),{ 'User-Agent': 'FlipsideStreamline' },{}) AS abi_data, SYSDATE() AS _inserted_timestamp
+    rn.contract_address, live.udf_api('GET', CONCAT('https://api.routescan.io/v2/network/mainnet/evm/', '43114', --avax C-chain ID
+    '/etherscan/api?module=contract&action=getabi&address=', contract_address, '&apikey=none'),{ 'User-Agent': 'FlipsideStreamline' },{}) AS abi_data, SYSDATE() AS _inserted_timestamp
 FROM
     row_nos rn
 WHERE

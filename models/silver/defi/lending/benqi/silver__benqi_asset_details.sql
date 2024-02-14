74bd50287efb5061e3b96e7eb32eff8a83f6912a
Matt Romano <42412983+mattromano@users.noreply.github.com>
2024-02-14 12:13:56 -0800

    AN-3834/add-evem-lending-protocols  (#221)
    * add all avax lending silver + gold
    
    * asset details fix + general docs
    
    * aave chain quick fix
    
    * update modified columns

---
+++ models/silver/defi/lending/benqi/silver__benqi_asset_details.sql
@@ -0,0 +1,106 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = "block_number",
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['reorg','curated']
+) }}
+
+WITH 
+contracts AS (
+    SELECT
+        *
+    FROM
+        {{ ref('silver__contracts') }}
+),
+log_pull AS (
+
+    SELECT
+        l.tx_hash,
+        l.block_number,
+        l.block_timestamp,
+        l.contract_address,
+        C.token_name,
+        C.token_symbol,
+        C.token_decimals,
+        l._inserted_timestamp,
+        l._log_id
+    FROM
+        {{ ref('silver__logs') }} l 
+    LEFT JOIN
+        contracts c
+    ON
+        l.contract_address = c.contract_address
+    WHERE
+        topics [0] :: STRING = '0x7ac369dbd14fa5ea3f473ed67cc9d598964a77501540ba6751eb0b3decf5870d'
+        AND 
+            TOKEN_NAME LIKE '%Benqi %'
+
+{% if is_incremental() %}
+AND l._inserted_timestamp >= (
+    SELECT
+        MAX(
+            _inserted_timestamp
+        ) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+traces_pull AS (
+    SELECT
+        from_address AS token_address,
+        to_address AS underlying_asset
+    FROM
+        {{ ref('silver__traces') }}
+    WHERE
+        tx_hash IN (
+            SELECT
+                tx_hash
+            FROM
+                log_pull
+        )
+        AND identifier = 'STATICCALL_0_2'
+),
+contract_pull AS (
+    SELECT
+        l.tx_hash,
+        l.block_number,
+        l.block_timestamp,
+        l.contract_address,
+        token_name,
+        token_symbol,
+        token_decimals,
+        CASE 
+            WHEN TOKEN_NAME = 'Benqi AVAX' THEN '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7' 
+            ELSE t.underlying_asset
+        END AS underlying_asset,
+        l._inserted_timestamp,
+        l._log_id
+    FROM
+        log_pull l
+        LEFT JOIN traces_pull t
+        ON l.contract_address = t.token_address qualify(ROW_NUMBER() over(PARTITION BY l.contract_address
+    ORDER BY
+        block_timestamp ASC)) = 1
+)
+SELECT
+    l.tx_hash,
+    l.block_number,
+    l.block_timestamp,
+    l.contract_address AS token_address,
+    l.token_name,
+    l.token_symbol,
+    l.token_decimals,
+    l.underlying_asset AS underlying_asset_address,
+    C.token_name AS underlying_name,
+    C.token_symbol AS underlying_symbol,
+    C.token_decimals AS underlying_decimals,
+    l._inserted_timestamp,
+    l._log_id
+FROM
+    contract_pull l
+    LEFT JOIN contracts C
+    ON C.contract_address = l.underlying_asset
+WHERE
+     l.token_name IS NOT NULL

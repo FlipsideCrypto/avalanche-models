e55083b61340dd0dd9d175020541b2ce4162fc21
drethereum <71602799+drethereum@users.noreply.github.com>
2024-05-01 13:05:41 -0600

    AN-4705/prices-v2 (#243)
    * new prices models and docs
    
    * references
    
    * new names
    
    * addr desc

--- models/silver/core/silver__native_transfers.sql
+++ models/silver/core/silver__native_transfers.sql
@@ -99,7 +99,7 @@ SELECT
     '{{ invocation_id }}' AS _invocation_id
 FROM
     avax_base A
-    LEFT JOIN {{ ref('silver__hourly_prices_priority') }}
+    LEFT JOIN {{ ref('silver__complete_token_prices') }}
     ON DATE_TRUNC(
         'hour',
         A.block_timestamp

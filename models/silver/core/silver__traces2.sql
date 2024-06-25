504255b0ffb1eadb773f81149930b2eaa2e1e70d
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-06-25 19:03:48 -0400

    regular logic (#253)
    * regular logic
    
    * main

--- models/silver/core/silver__traces2.sql
+++ models/silver/core/silver__traces2.sql
@@ -5,7 +5,8 @@
     unique_key = "block_number",
     cluster_by = ['modified_timestamp::DATE','partition_key'],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
-    tags = ['traces2']
+    full_refresh = false,
+    tags = ['non_realtime']
 ) }}
 
 WITH bronze_traces AS (
@@ -19,20 +20,15 @@ WITH bronze_traces AS (
     FROM
 
 {% if is_incremental() %}
-{{ ref('bronze__streamline_FR_traces') }}
+{{ ref('bronze__streamline_traces') }}
 WHERE
-    _partition_by_block_id BETWEEN (
+    _inserted_timestamp >= (
         SELECT
-            MAX(partition_key)
+            MAX(_inserted_timestamp) _inserted_timestamp
         FROM
             {{ this }}
     )
-    AND (
-        SELECT
-            MAX(partition_key) + 3000000
-        FROM
-            {{ this }}
-    )
+    AND DATA :result IS NOT NULL
 {% else %}
     {{ ref('bronze__streamline_FR_traces') }}
 WHERE

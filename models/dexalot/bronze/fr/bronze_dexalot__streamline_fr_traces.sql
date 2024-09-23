6f852486ee7f039187b965dee0dd306bd32dc094
Austin <austin@flipsidecrypto.com>
2024-09-23 16:26:23 -0400

    fr

--- models/dexalot/bronze/fr/bronze_dexalot__streamline_fr_traces.sql
+++ models/dexalot/bronze/fr/bronze_dexalot__streamline_fr_traces.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = 'view'
 ) }}
-{{ fsc_evm.streamline_external_table_FR_query(
+{{ fsc_evm.streamline_external_table_fr_query(
     model = "dexalot_traces",
     partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER )"
 ) }}

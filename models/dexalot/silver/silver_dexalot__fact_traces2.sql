16c1ecd18ba9fa10179983747437aa292cec412e
Austin <austin@flipsidecrypto.com>
2024-09-23 16:53:25 -0400

    regular logic

--- models/dexalot/silver/silver_dexalot__fact_traces2.sql
+++ models/dexalot/silver/silver_dexalot__fact_traces2.sql
@@ -4,13 +4,13 @@
     unique_key = ['block_number'],
     cluster_by = "block_timestamp::date",
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(block_number,tx_hash,from_address,to_address,trace_address,type,identifier), SUBSTRING(input,output,type,trace_address,identifier,error_reason,revert_reason)",
-    full_refresh = false
+    full_refresh = false,
+    tags = ['dexalot_non_realtime']
 ) }}
 
 {{ fsc_evm.gold_traces_v2(
     full_reload_start_block = 25000000,
     full_reload_blocks = 2000000,
     schema_name = 'silver_dexalot',
-    full_reload_mode = true,
     uses_tx_status = true
 ) }}

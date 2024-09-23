7dd9dadbc69f7242b69cc5af7446f070223e588a
Austin <austin@flipsidecrypto.com>
2024-09-23 16:16:49 -0400

    version

--- models/dexalot/silver/silver_dexalot__fact_traces2.sql
+++ models/dexalot/silver/silver_dexalot__fact_traces2.sql
@@ -3,7 +3,8 @@
     incremental_strategy = 'delete+insert',
     unique_key = ['block_number'],
     cluster_by = "block_timestamp::date",
-    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(block_number,tx_hash,from_address,to_address,trace_address,type,identifier), SUBSTRING(input,output,type,trace_address,identifier,error_reason,revert_reason)"
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(block_number,tx_hash,from_address,to_address,trace_address,type,identifier), SUBSTRING(input,output,type,trace_address,identifier,error_reason,revert_reason)",
+    full_refresh = false
 ) }}
 
 {{ fsc_evm.gold_traces_v2(

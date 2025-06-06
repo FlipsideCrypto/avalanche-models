1757fdc14ccc49dd0d866e3166fd4ae75809033e
San Yong <22216004+SanYongxie@users.noreply.github.com>
2025-06-06 15:41:49 +0800

    convert dexalot to fact logs and new topic

--- models/silver/defi/dex/dexalot/silver_dex__dexalot_swaps.sql
+++ models/silver/defi/dex/dexalot/silver_dex__dexalot_swaps.sql
@@ -17,21 +17,21 @@ WITH base_swaps AS (
         tx_hash,
         event_index,
         contract_address,
-        decoded_log,
-        TRY_TO_NUMBER(
-            decoded_log :destAmount :: STRING
-        ) AS destAmount,
-        decoded_log :destAsset :: STRING AS destAsset,
-        TRY_TO_NUMBER(
-            decoded_log :destChainId :: STRING
-        ) AS destChainId,
-        decoded_log :destTrader :: STRING AS destTrader,
-        decoded_log :nonceAndMeta :: STRING AS nouceAndMeta,
-        TRY_TO_NUMBER(
-            decoded_log :srcAmount :: STRING
-        ) AS srcAmount,
-        decoded_log :srcAsset :: STRING AS srcAsset,
-        decoded_log :taker :: STRING AS taker,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        topic_1 AS nonceAndMeta,
+        CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS taker,
+        CONCAT('0x', SUBSTR(segmented_data [1] :: STRING, 25, 40)) AS destTrader,
+        TRY_TO_NUMBER(utils.udf_hex_to_int(segmented_data [2] :: STRING)) AS destChainId,
+        CASE
+            WHEN CONCAT('0x', SUBSTR(segmented_data [3] :: STRING, 25, 40)) = '0x0000000000000000000000000000000000000000' THEN '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7'
+            ELSE CONCAT('0x', SUBSTR(segmented_data [3] :: STRING, 25, 40))
+        END AS srcAsset,
+        CASE
+            WHEN CONCAT('0x', SUBSTR(segmented_data [4] :: STRING, 25, 40)) = '0x0000000000000000000000000000000000000000' THEN '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7'
+            ELSE CONCAT('0x', SUBSTR(segmented_data [4] :: STRING, 25, 40))
+        END AS destAsset,
+        TRY_TO_NUMBER(utils.udf_hex_to_int(segmented_data [5] :: STRING)) AS srcAmount,
+        TRY_TO_NUMBER(utils.udf_hex_to_int(segmented_data [6] :: STRING)) AS destAmount,
         CONCAT(
             tx_hash,
             '-',
@@ -39,10 +39,13 @@ WITH base_swaps AS (
         ) AS _log_id,
         modified_timestamp
     FROM
-        {{ ref('core__ez_decoded_event_logs') }}
+        {{ ref('core__fact_event_logs') }}
     WHERE
         contract_address = '0xeed3c159f3a96ab8d41c8b9ca49ee1e5071a7cdd'
-        AND topic_0 = '0x68eb6d948c037c94e470f9a5b288dd93debbcd9342635408e66cb0211686f7f7'
+        AND topic_0 IN (
+            '0x68eb6d948c037c94e470f9a5b288dd93debbcd9342635408e66cb0211686f7f7',
+            '0xfeb087be954e9eb692f863466081925668f8f5214f5c1d1a28438df811cbf042'
+        )
         AND destChainId = 43114
         AND tx_succeeded
         AND event_removed = FALSE

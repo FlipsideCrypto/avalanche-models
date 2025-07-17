7855dad1dfa5e3a75d2af0455a8ce5dbe26925a4
sam <sam@flipsidecrypto.com>
2025-07-17 14:07:11 +0800

    fix dexalot docs

--- models/silver/defi/bridge/silver_bridge__complete_bridge_activity.sql
+++ models/silver/defi/bridge/silver_bridge__complete_bridge_activity.sql
@@ -190,6 +190,44 @@ WHERE
     )
 {% endif %}
 ),
+
+everclear AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        tx_hash,
+        event_index,
+        bridge_address,
+        event_name,
+        platform,
+        version,
+        sender,
+        receiver,
+        destination_chain_receiver,
+        destination_chain_id :: STRING AS destination_chain_id,
+        destination_chain,
+        token_address,
+        NULL AS token_symbol,
+        amount_unadj,
+        _log_id AS _id,
+        inserted_timestamp AS _inserted_timestamp
+    FROM
+        {{ ref('silver_bridge__everclear') }}
+
+{% if is_incremental() and 'everclear' not in var('HEAL_MODELS') %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(_inserted_timestamp) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+        FROM
+            {{ this }}
+    )
+{% endif %}
+),
+
 eywa AS (
     SELECT
         block_number,
@@ -723,6 +761,11 @@ all_protocols AS (
     SELECT
         *
     FROM
+        everclear
+    UNION ALL
+    SELECT
+        *
+    FROM
         eywa
     UNION ALL
     SELECT
@@ -821,7 +864,8 @@ complete_bridge_activity AS (
                 'chainlink-ccip-v1',
                 'layerzero-v2',
                 'stargate-v2',
-                'avalanche_native_bridge-v2'
+                'avalanche_native_bridge-v2',
+                'everclear-v1'
             ) THEN destination_chain_id :: STRING
             WHEN d.chain_id IS NULL THEN destination_chain_id :: STRING
             ELSE d.chain_id :: STRING
@@ -841,7 +885,8 @@ complete_bridge_activity AS (
                 'chainlink-ccip-v1',
                 'layerzero-v2',
                 'stargate-v2',
-                'avalanche_native_bridge-v2'
+                'avalanche_native_bridge-v2',
+                'everclear-v1'
             ) THEN LOWER(destination_chain)
             WHEN d.chain IS NULL THEN LOWER(destination_chain)
             ELSE LOWER(
